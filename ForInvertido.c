ea7aeb2c7980e9fc23782401ca78e935ae97a509
Marco <maphy1707@gmail.com>
2023-07-24 22:34:22 -0600

    For invertido

---
+++ ForInvertido.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+    int secuenciaInvertida = 100;
+    int contador = 0;
+
+    for(contador = 0; contador <= 99; contador++){
+        //printf("Secuencia 100 - 0: %i", secuenciaInvertida);
+        //secuenciaInvertida-1;
+        printf("%i", contador);
+    }
+    
+    system("PAUSE");
+    return 0;
+}
